fde3f7bce2b4032c02ba0b1f600de81faccc9ac3
Sabiya Aman <mssabiyaaman@gmail.com>
2022-03-17 20:33:19 -0400

    1st commit

---
+++ src/SamplePackage/HelloWorld.java
@@ -0,0 +1,12 @@
+package SamplePackage;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    static String phrase = "HelloWorld";
+
+    public static void main(String[] args) {
+         System.out.println(phrase);
+    }
+}
